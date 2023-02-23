0921a8e1a90cf4b45d59b7c0c366870f7835653e
bca <bca@Bcalab4-m07>
2023-02-23 08:47:13 +0530

    First commit

---
+++ Casetools/src/MilkCrates.java
@@ -0,0 +1,19 @@
+
+public class MilkCrates {
+	int num_crates;
+	int bottles_per_crate;
+	double unit_cost_bottle;
+	
+	MilkCrates(int num_crates,double unit_cost_bottle)
+	{
+		this.num_crates= num_crates;
+		this.unit_cost_bottle=unit_cost_bottle;
+		bottles_per_crate=25;
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
